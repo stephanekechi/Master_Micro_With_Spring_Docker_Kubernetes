f4599d5b563181ddb2a0a60e0173dee836bea32d
Nganou Wafo, Stephane <stephane.nganou.wafo@ausy-technologies.de>
2022-08-07 13:55:15 +0200

    add loans module

--- accounts/src/main/java/com/bank/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/bank/accounts/controller/AccountsController.java
@@ -3,14 +3,19 @@ package com.bank.accounts.controller;
 import com.bank.accounts.model.Accounts;
 import com.bank.accounts.model.Customer;
 import com.bank.accounts.repository.AccountsRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author Stephane Nganou
  */
 @RestController
+@RequestMapping("/accounts")
+@Slf4j
 public class AccountsController {
 
     private final AccountsRepository accountsRepository;
@@ -20,8 +25,10 @@ public class AccountsController {
         this.accountsRepository = accountsRepository;
     }
 
+    @PostMapping("/")
     public Accounts getAccountsDetails(@RequestBody Customer customer) {
 
+        log.info("Requesting account '/' with: {}", customer);
         Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
 
         if (null != accounts) {
