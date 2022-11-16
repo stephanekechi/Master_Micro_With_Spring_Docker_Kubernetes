ca7a7bdf501f768ee23d31cd940d933de31e597c
Nganou Wafo, Stephane <stephane.nganou.wafo@ausy-technologies.de>
2022-11-16 21:54:40 +0100

    changes for ratelimiter

--- accounts/src/main/java/com/bank/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/bank/accounts/controller/AccountsController.java
@@ -13,7 +13,7 @@ import com.bank.accounts.service.client.LoansFeignClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -48,16 +48,13 @@ public class AccountsController {
         this.cardsFeignClient = cardsFeignClient;
     }
 
-    @PostMapping("/")
-    public Accounts getAccountsDetails(@RequestBody Customer customer) {
-
-        log.info("Requesting account '/' with: {}", customer);
-        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
-
-        if (null != accounts) {
-            return accounts;
-        }
-        return null;
+    /**
+     * Method for testing RateLimiter Pattern
+     */
+    @GetMapping("/sayHello")
+    @RateLimiter(name = "sayHello", fallbackMethod = "sayHelloFallback")
+    public String sayHello() {
+        return "Hello, Welcome to Greatness Bank";
     }
 
     @GetMapping("/properties")
@@ -68,6 +65,18 @@ public class AccountsController {
                 accountsServiceConfig.getMailDetails(), accountsServiceConfig.getActiveBranches());
 
         return objectWriter.writeValueAsString(properties);
+    }
+
+    @PostMapping("/")
+    public Accounts getAccountsDetails(@RequestBody Customer customer) {
+
+        log.info("Requesting account '/' with: {}", customer);
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+
+        if (null != accounts) {
+            return accounts;
+        }
+        return null;
     }
 
     @PostMapping("/myCustomerDetails")
@@ -88,6 +97,10 @@ public class AccountsController {
 
         return new CustomerDetails(accounts, null, null);
 
+    }
+
+    private String sayHelloFallback(Throwable t) {
+        return "Hi, here sayHelloFallback. Welcome to Greatness bank";
     }
 
 }
