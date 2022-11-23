7a9423eb48a35abbc2d5dd0e8432ffb0153c5243
Nganou Wafo, Stephane <stephane.nganou.wafo@ausy-technologies.de>
2022-11-23 17:31:13 +0100

    fixing deployment to consider gatewayserver application

--- gatewayserver/src/main/java/com/bank/gatewayserver/GatewayserverApplication.java
+++ gatewayserver/src/main/java/com/bank/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,14 @@ package com.bank.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayserverApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayserverApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayserverApplication.class, args);
+    }
 
 }
