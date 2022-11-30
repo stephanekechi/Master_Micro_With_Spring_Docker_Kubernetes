3737bb8e885e2bfa1d0d3c28ede5b09f3baf7970
Nganou Wafo, Stephane <stephane.nganou.wafo@ausy-technologies.de>
2022-11-30 04:52:55 +0100

    implementing cross concern in Gateway

---
+++ gatewayserver/src/main/java/com/bank/gatewayserver/filter/TraceFilter.java
@@ -0,0 +1,44 @@
+package com.bank.gatewayserver.filter;
+
+import com.bank.gatewayserver.utility.FilterUtility;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Order(1) // prescribe the order of filters.
+@Slf4j
+@Component
+public class TraceFilter implements GlobalFilter {
+
+    @Autowired
+    private FilterUtility filterUtility;
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
+        if (isCorrelationIdPresent(requestHeaders)) {
+            log.info("GreatnessBank-correlation-id found in tracing filter: {}. ", filterUtility.getCorrelationId(requestHeaders));
+        } else {
+            String correlationId = generateCorrelationId();
+            exchange = filterUtility.setCorrelationId(exchange, correlationId);
+            log.info("GreatnessBank-correlation-id generated in tracing filter: {}.", correlationId);
+        }
+
+        return chain.filter(exchange);
+    }
+
+    private boolean isCorrelationIdPresent(HttpHeaders requestHeaders) {
+
+        return filterUtility.getCorrelationId(requestHeaders) != null;
+    }
+
+    private String generateCorrelationId() {
+        return java.util.UUID.randomUUID().toString();
+    }
+}
